2beb48334baa05280b750fc3c75e1bbc2d91f6a4
johnbuenodev <johnbuenodev@gmail.com>
2022-07-31 14:45:59 -0300

    exemplo simples de componente, uso de grupo de estilo styles e no jsx, uso de image react native

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Component, useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+
+  const [titulo, setTitulo] = useState('primeiro titulo');
+
+  function alterTitulo() {
+   setTitulo('Novo titulo');
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <>
+    <View style={{flex:1, flexDirection:'column',marginBottom: '64px', alignItems:'center', justifyContent:'flex-start',padding:16}}>
+      <Text>Curso Sujeito programador</Text>
+      <Text>React Native</Text>
+      <Text style={{ color:'#3344ff', fontSize: 25, margin:16}}>
+        Hello World!!
+      </Text>
+      <Jobs largura={150} altura={150} name={'Steve Jobs'}/>
+      <View>
+        <Text>State:</Text>
+        <Text>{titulo}</Text>
+        <TouchableOpacity style={styles.buttonAlter} onPress={alterTitulo}>
+          Trocar Nome
+        </TouchableOpacity>
+      </View>
     </View>
+    <View style={{flex:1,flexDirection:'column',backgroundColor:'#445588'}}>
+       <View style={{width:45,height:45,backgroundColor:'#995577'}}></View>
+       <View style={{flex:1,backgroundColor:'red'}}></View>
+       <View style={{flex:2,backgroundColor:'green'}}></View>
+       <View style={{flex:0.5,backgroundColor:'violet'}}></View>
+       <View style={{flex:0.75,backgroundColor:'magento'}}></View>
+    </View>
+    <StatusBar style="auto" />
+    </>
   );
 }
 
@@ -17,4 +46,29 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  buttonAlter: {
+    backgroundColor: '#4B0082',
+    color: '#ffff',
+    fontWeight: 'bold',
+    padding: 16,
+    width:150,
+    alignItems:'center',
+    borderRadius: '6px'
+  }
 });
+
+class Jobs extends Component {
+  render() {
+    let img = "https://sujeitoprogramador.com/steve.png";
+
+    return (
+      <View>
+        <Image 
+         source={{ uri: img}}
+         style={{width: this.props.largura, height: this.props.altura}}
+        />
+        <Text>{this.props.name}</Text>
+      </View>
+    )
+  }
+}
